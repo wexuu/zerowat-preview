503cd6353aa37bb89f42b3a96ac20ba8781551b2
wexuu <kubawxl@gmail.com>
2026-09-14 19:51:39 +0200

    Preview build from zerowat-redesign@c884a2cd7932292a3fbe6d4bfaf3570c8b6fd6aa
    Opublikowane bezpośrednio z maszyny: pipeline nie może wgrać artefaktu, bo
    licznik magazynu konta stoi na limicie mimo usunięcia plików. Dodany .nojekyll,
    bez którego GitHub Pages pomija katalog _next i cały JavaScript zwraca 404.

---
+++ _next/static/chunks/app/dofinansowanie/page-04ae614fba58c89c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[68,160,404],{187:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=187)}),_N_E=n.O()}]);
